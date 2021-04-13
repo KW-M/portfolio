5d17c58a49698143bd696f8f571b1e5c5d920f99
Kyle Worcester-Moore <25kylworc@gmail.com>
2021-04-12 17:04:46 -0700

    Add Experimental Website Selector

--- main.js
+++ main.js
@@ -159,3 +159,47 @@ topBarElem.addEventListener('focusin', (event) => {
 topBarElem.addEventListener('focusout', (event) => {
     topBarElem.className = '';
 });
+
+function getGithubPages() {
+    let projectLinks = { 'UC Santa Cruz | Class Projects': [{ 'link': 'https://people.ucsc.edu/~kworcest/CS%20Programming%20Assignment%20Archive/', 'name': 'UCSC Class Projects', 'archived': false }] }
+    fetch('https://api.github.com/users/KW-M/repos?type=all&per_page=100').then(response => response.json()).then((repos) => {
+
+        for (let repo_indx = 0; repo_indx < repos.length; repo_indx++) {
+            const repo_obj = repos[repo_indx];
+            if (repo_obj.has_pages) {
+                let [user_name, repo_name] = repo_obj.full_name.split('/');
+                let linkList = projectLinks["Github | " + user_name] = projectLinks["Github | " + user_name] || []
+                linkList.push({ 'link': 'https://' + user_name + '.github.io/' + repo_name, 'name': repo_name, 'archived': repo_obj.archived });
+                //fetch(`https://api.github.com/repos/${repo_obj.full_name}/pages`).then(response => response.json()).then(json => ({ 'link': json.html_url, 'name': repo_obj.full_name, 'archived': repo_obj.archived })))
+            }
+        }
+        let selectElem = document.getElementById("project_pages_select");
+        selectElem.addEventListener("change", (ev) => {
+            window.open(ev.target.value);
+            ev.target.selectedIndex = 0;
+        });//window.open('')
+        // selectElem.innerHTML = "";
+        for (const label in projectLinks) {
+            if (Object.hasOwnProperty.call(projectLinks, label)) {
+                const linkList = projectLinks[label];
+                const optGroup = document.createElement('OPTGROUP');
+                optGroup.setAttribute("label", label)
+                for (let index = 0; index < linkList.length; index++) {
+                    const projectObj = linkList[index];
+                    const opt = document.createElement('OPTION')
+                    opt.text = projectObj.name;
+                    opt.value = projectObj.link;
+                    if (projectObj.archvied) opt.classList.add('faded_page');
+                    optGroup.appendChild(opt);
+                }
+                selectElem.appendChild(optGroup);
+            }
+        }
+
+
+        console.log(selectElem, projectLinks)
+    });
+
+    // fetch('https://api.github.com/users/KW-M/orgs').then(response => response.json()).then(console.log)
+}
+getGithubPages();
