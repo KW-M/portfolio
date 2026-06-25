2c7535aec83902f081bb94d8b18e1bdc708759af
Kyle Worcester-Moore <16263357+KW-M@users.noreply.github.com>
2026-06-24 21:47:40 -0400

    Revision of pages and addtion of draft ISMS page

--- src/routes/(main)/project/ROV-Web/+page.server.ts
+++ src/routes/(main)/project/ROV-Web/+page.server.ts
@@ -3,4 +3,5 @@ import Img2 from "./carousel/2.jpg?lqip";
 import Img3 from "./carousel/3.jpg?lqip";
 import Img4 from "./carousel/4.png?lqip";
 import Img5 from "./carousel/5.png?lqip";
-export const _mediaSlides = [Img1, Img2, Img3, Img4, Img5].map((lqip) => ({ type: "img", ...lqip }));
+import Img6 from "./carousel/6.png?lqip";
+export const _mediaSlides = [Img1, Img2, Img3, Img4, Img5, Img6].map((lqip) => ({ type: "img", ...lqip }));
---
+++ unpublished projects/ISMS/+page.server.ts
@@ -0,0 +1,6 @@
+import Image1 from "./carousel/1.png?lqip";
+import Image2 from "./carousel/2.png?lqip";
+import Image3 from "./carousel/3.png?lqip";
+import Image4 from "./carousel/4.png?lqip";
+import Image5 from "./carousel/5.png?lqip";
+export const _mediaSlides = [Image1, Image2, Image3, Image4, Image5].map((lqip) => ({ type: "img", ...lqip }));
